63a4f5b3293cda7767b3b42746e7f51462b61f20
Júlio Xavier <115498048+Julioxli@users.noreply.github.com>
2023-03-13 19:21:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-// console.log('oi')
-
-// const readlineSync = require('readline-sync');
-
-// let ask = parseFloat(readlineSync.question('Voce gostaria de adicionar codigos css?'));
-// const tabela = [];
-
-// while (ask != 'sair') {
-//     const add = parseFloat(readlineSync.question('Adicione o codigo?'));
-//     tabela.push(add);
-//     if (add == 'sair') {
-//         tabela.pop('sair');
-//         console.log(tabela.sort());
-//         console.log(tabela);
-//         break
-//     }   
-// }
-
-
 const readlineSync = require('readline-sync');
 
 let ask = readlineSync.question('Voce gostaria de adicionar codigos css?');
